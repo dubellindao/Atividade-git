c00e4c79efb1cff356ed567a02205b4d3c8230c9
dubellindao <emanuel.brito@escolar.ifrn.edu.br>
2024-08-01 18:52:48 -0300

    adicionando meu template(emanuel)

---
+++ criacao_url/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from criacao_url.views import my_function
+
+urlpatterns = [
+    path('', my_function,name='my_function') ,
+]
--- criacao_url/views.py
+++ criacao_url/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def my_function(request):
+    return render(request, 'criacao_url/PaginaEmanuel.html')
